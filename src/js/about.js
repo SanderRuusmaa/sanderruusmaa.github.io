280f67ce70792b8cbaccef33806a88652960202b
SanderRuusmaa <56891593+SanderRuusmaa@users.noreply.github.com>
2020-12-19 20:38:06 +0200

    about.html animations added

--- src/js/about.js
+++ src/js/about.js
@@ -6,16 +6,59 @@ $( document ).ready(function(){
 });
 })
 
-  gsap.registerPlugin(ScrollTrigger);
-  gsap.to(".contact-info",{
-    scrollTrigger: {
-      trigger:".contact-info",
-      start:"0 75%",
-      end:"+=0",
-      toggleActions: "play none none reset",
-      markers:true,
-    },
-    "transform":"translateX(0)"
-  });
+gsap.registerPlugin(ScrollTrigger);
 
-  gsap.fromTo(".intro", {opacity:0}, {opacity:1,delay:0.3, duration:2, "transition":"ease-out"});
+//contact info slide in
+gsap.to(".contact-info",{
+  scrollTrigger: {
+    trigger:".focus",
+    start:"90% 95%",
+    end:"+=0",
+    toggleActions: "play none none reset",
+  },
+  "transform":"translateX(0)"
+});
+
+//parallax pattern
+gsap.to(".established",{
+  scrollTrigger:{
+      trigger: ".header",
+      start:"top",
+      end: "bottom",
+      scrub:1.5,
+  },
+  "background-position":"2% 20%"
+})
+
+//defining triggers in established section
+let t1 = gsap.timeline({
+  scrollTrigger: {
+      trigger: '#est',
+      start: "0% 50%",
+  }
+});
+
+let t2 = gsap.timeline({
+  scrollTrigger: {
+      trigger: '.est-story',
+      start: "0% 50%",
+  }
+});
+
+let t3 = gsap.timeline({
+  scrollTrigger: {
+      trigger: '#whatdowedo',
+      start: "0% 50%",
+  }
+});
+
+t1.from("#est", {scale:0.9, opacity:0, duration: 1});
+t2.from(".est-story", {opacity:0,duration:1});
+t3.from("#whatdowedo", {scale:0.2,opacity:0, duration:1})
+t3.from("#abouttext1",{opacity:0, delay:0.1, duration:0.7})
+t3.from("#abouttext2",{opacity:0, delay:0.1, duration:0.7})
+t3.from("#abouttext3",{opacity:0, delay:0.1, duration:0.7})
+t3.from("#oliver", {"transform":"translateX(100%)"})
+
+//about us intro text fade in
+gsap.fromTo(".intro", {opacity:0}, {opacity:1,delay:0.3, duration:2, "transition":"ease-out"});
